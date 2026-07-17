db0c0e6d2c2be24efab706abab42c31dba5ced4c
ShadowQueen06 <malk.lol140@gmail.com>
2026-07-17 16:03:45 +0200

    Add files via upload

--- handlers/eventHandler.js
+++ handlers/eventHandler.js
@@ -1,1 +1,10 @@
-
+const fs = require('node:fs');
+const path = require('node:path');
+module.exports = function loadEvents(client) {
+  const dir = path.join(__dirname, '..', 'events');
+  for (const file of fs.readdirSync(dir).filter(f => f.endsWith('.js'))) {
+    const event = require(path.join(dir, file));
+    if (!event?.name || typeof event.execute !== 'function') continue;
+    event.once ? client.once(event.name, (...args) => event.execute(...args, client)) : client.on(event.name, (...args) => event.execute(...args, client));
+  }
+};
