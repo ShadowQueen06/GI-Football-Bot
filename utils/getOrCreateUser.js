31d11e431c7db53a354a883857e0ad29d69a92e5
ShadowQueen06 <malk.lol140@gmail.com>
2026-07-17 19:03:22 +0200

    getOrCreateUser.js

--- utils/getOrCreateUser.js
+++ utils/getOrCreateUser.js
@@ -1,1 +1,31 @@
-const User=require('../database/models/User');const config=require('../config/config');module.exports=async function(discordId,guildId){return User.findOneAndUpdate({discordId,guildId},{$setOnInsert:{discordId,guildId,coins:config.starterCoins}},{upsert:true,new:true,setDefaultsOnInsert:true});};
+const User = require('../database/models/User');
+const config = require('../config/config');
+
+module.exports = async function getOrCreateUser(discordId, guildId) {
+  let user = await User.findOne({
+    discordId,
+    guildId
+  });
+
+  if (!user) {
+    user = await User.create({
+      discordId,
+      guildId,
+      giPoints: config.starterPoints
+    });
+
+    return user;
+  }
+
+  // ينقل رصيد coins القديم إلى GI Points حتى ما يضيع
+  if (typeof user.giPoints !== 'number') {
+    user.giPoints =
+      typeof user.coins === 'number'
+        ? user.coins
+        : config.starterPoints;
+
+    await user.save();
+  }
+
+  return user;
+};
