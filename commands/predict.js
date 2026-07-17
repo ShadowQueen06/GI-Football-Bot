831dc94f4d7733405bc1e4ec81388bc0355016d8
ShadowQueen06 <malk.lol140@gmail.com>
2026-07-17 21:29:40 +0200

    predict.js

--- commands/predict.js
+++ commands/predict.js
@@ -1,1 +1,10 @@
-// محفوظ للتحديثات القادمة من GI Football.
+module.exports = {
+    name: "توقع",
+    aliases: ["predict"],
+
+    async execute(message) {
+        return message.reply(
+            "❌ التوقعات تتم عن طريق زر **توقع النتيجة** الموجود أسفل رسالة المباراة، وليس بالأوامر."
+        );
+    }
+};
