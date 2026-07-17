e6fec6d7f2046ddf31f578cbd99ede30c1cdcc4a
ShadowQueen06 <malk.lol140@gmail.com>
2026-07-17 19:07:33 +0200

    balance.js

--- commands/balance.js
+++ commands/balance.js
@@ -1,1 +1,12 @@
-const getUser=require('../utils/getOrCreateUser');module.exports={name:'رصيدي',aliases:['فلوسي','عملاتي'],async execute(message){const user=await getUser(message.author.id,message.guild.id);await message.reply(`🪙 رصيدك: **${user.coins} GI Coins**`);}};
+const getUser = require('../utils/getOrCreateUser');
+
+module.exports = {
+  name: 'رصيدي',
+  aliases: ['فلوسي', 'عملاتي'],
+
+  async execute(message) {
+    const user = await getUser(message.author.id, message.guild.id);
+
+    await message.reply(`💰 رصيدك: **${user.giPoints} GP**`);
+  }
+};
