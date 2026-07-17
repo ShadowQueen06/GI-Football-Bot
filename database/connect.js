f95a44e699cb5f905326c259567528defb12e846
ShadowQueen06 <malk.lol140@gmail.com>
2026-07-17 16:01:24 +0200

    Add files via upload

--- database/connect.js
+++ database/connect.js
@@ -1,1 +1,2 @@
-
+const mongoose=require('mongoose');
+module.exports=async function(uri){mongoose.connection.on('connected',()=>console.log('✅ MongoDB متصل'));mongoose.connection.on('error',e=>console.error('MongoDB:',e.message));await mongoose.connect(uri,{serverSelectionTimeoutMS:15000});};
